9f8dbaaea8119a6757450daeac6a6ef877fa8f60
Sherstpasha <sherstpasha99@gmail.com>
2023-08-13 11:37:21 +0700

    .

---
+++ tests/test_base.py
@@ -0,0 +1,21 @@
+from thefittest.base._ea import LastBest
+
+
+def test_lastbest():
+    lastbest = LastBest()
+
+    counter_1 = lastbest._no_increase_counter
+
+    lastbest._update(1)
+    counter_2 = lastbest._no_increase_counter
+
+    lastbest._update(1)
+    counter_3 = lastbest._no_increase_counter
+    lastbest._update(2)
+    counter_4 = lastbest._no_increase_counter
+
+    assert counter_1 == counter_2
+    assert counter_2 == counter_3 - 1
+    assert counter_4 == 0
+
+
